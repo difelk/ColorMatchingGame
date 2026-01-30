a0410e027852875c09b9e32c5d0c518ce2a60d4e
difelk <dilshan.f@theavo.net>
2026-01-30 22:46:09 +0530

    Initial Commit

---
+++ ColorMatchingGame/ColorMatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatchingGameApp.swift
+//  ColorMatchingGame
+//
+//  Created by dilshan fernando on 2026-01-30.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
